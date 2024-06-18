25103180307ad45492393e64f63806b3c845b52e
Piotrek <252760@edu.p.lodz.pl>
2024-06-18 16:39:19 +0200

    basic program

--- operation.cpp
+++ operation.cpp
@@ -1,3 +1,25 @@
 //
 // Created by Piotr on 18.06.2024.
 //
+
+#include <iostream>
+using namespace std;
+double add(double liczba1, double liczba2) {
+    return liczba1 + liczba2;
+}
+
+double sub(double liczba1, double liczba2) {
+    return liczba1 - liczba2;
+}
+
+double multiply(double liczba1, double liczba2) {
+    return liczba1 * liczba2;
+}
+
+double div(double liczba1, double liczba2) {
+    if (liczba2 == 0) {
+
+        return 0;
+    }
+    return liczba1 / liczba2;
+}
