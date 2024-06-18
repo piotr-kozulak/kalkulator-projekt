d953970b54ccb5680b62cf9795ca9c42335109b5
Piotrek <252760@edu.p.lodz.pl>
2024-06-18 14:33:18 +0200

    basic menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
+#include "operation.h"
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double number1, number2;
+    char operation;
+    char next;
+    do {
+        cout << "Enter first number: ";
+        cin >> number1;
+
+        cout << "Enter operator (+, -, *, /): ";
+        cin >> operation;
+
+        cout << "Enter second number: ";
+        cin >> number2;
+
+        double result;
+
+        switch (operation) {
+            case '+':
+                result = add(number1, number2);
+                break;
+            case '-':
+                result = sub(number1, number2);
+                break;
+            case '*':
+                result = multiply(number1, number2);
+                break;
+            case '/':
+                result = dev(number1, number2);
+                break;
+            default:
+                cout << "Unknown operator!" << endl;
+                continue; // Pominąć tę iterację pętli
+        }
+
+        cout << "Result: " << result << endl;
+
+        cout << "Do you want continue? (y/n): ";
+
+        cin >> next;
+    } while (next != 'n');
+
+    cout << "Good bye" << endl;
+
     return 0;
 }
